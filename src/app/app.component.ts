194770188a7140e093bd840e5437c2b53fb2884f
Ahmed Abdo <devahmedabdo@gmail.com>
2023-04-11 17:59:57 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,74 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'COUP-CREATIVES';
+export class AppComponent implements OnInit {
+  title = 'obey-digital';
+  activeMenu: boolean = false;
+  reflectActiveMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
+  services: any = [
+    {
+      name: `Social Media 
+      Management`,
+      icon: 'vector.svg',
+      color: 'black',
+      dis: `Let us take care of 
+      everything from creating 
+      content, scheduling
+      posting and engaging 
+      with your community`,
+    },
+    {
+      name: `Digital 
+      Marketing`,
+      icon: 'facebook.svg',
+      color: 'yellow',
+      dis: `Get more patients in the door with 
+      our lead generation solutions 
+      tailored to the dental industry 
+      and designed to drive real results.`,
+    },
+    {
+      name: `Content 
+      Creation`,
+      icon: 'message.svg',
+      color: 'gray',
+      dis: `Get more patients in the door with 
+      our lead generation solutions 
+      tailored to the dental industry 
+      and designed to drive real results.`,
+    },
+  ];
+  links: any[] = [
+    {
+      link: ``,
+      img: `phone.png`,
+    },
+    {
+      link: ``,
+      img: `mail.png`,
+    },
+    {
+      link: ``,
+      img: `instagram.png`,
+    },
+    {
+      link: ``,
+      img: `twitter.png`,
+    },
+    {
+      link: ``,
+      img: `tiktok.png`,
+    },
+  ];
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      this.activeMenu = false;
+    });
+  }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EBEBEB",
+        yellow: "#FFD900",
+        gray: "#D9D9D9",
+      },
+    },
+  },
+  plugins: [],
+};
